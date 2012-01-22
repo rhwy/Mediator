0a6f887a49496925086b5aa6b8914e1fc1f33606
Rui <github@rui.fr>
2012-01-22 22:47:14 +0100

    add forgetted folders to auto build

--- Common/CommonAssemblyInfo.cs
+++ Common/CommonAssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyConfiguration("")]
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.0.2121.0")]
+[assembly: AssemblyFileVersion("1.0.2121.0")]
