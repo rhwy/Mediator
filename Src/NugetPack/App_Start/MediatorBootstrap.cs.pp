e0a7d9ef12631cc53e7ff9af62199c7af1b82406
Rui <github@rui.fr>
2012-01-30 17:03:48 +0000

    updating bootstrap  helper with additional instructions

--- Src/NugetPack/App_Start/MediatorBootstrap.cs.pp
+++ Src/NugetPack/App_Start/MediatorBootstrap.cs.pp
@@ -6,7 +6,21 @@ using StructureMap;
 namespace $rootnamespace$.App_Start {
     public static class MediatorBootsrap {
         public static void Start() {
+        	//register the bus for simple messages (of string)
+        	//dont forget to add a bus for each custom message
             MediatorBus.RegisterForType<string>();
+
+            //add a route for Mediator in your routes configuration:
+            /*
+            routes.Add(
+                "mediator",
+                new Route(
+                    "mediator/{type}/{name}",
+                    new RouteValueDictionary(new { type = "string", name = "default" }),
+                    new EventBusRouteHandler()));
+	
+
+            */
         }
     }
 }
